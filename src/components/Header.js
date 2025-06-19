00f745750b5f443c4a08e71211a06eeb7c1f1138
Loki207 <logesh2002l@gmail.com>
2025-06-19 19:51:17 +0530

    modifed header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '..components/tt.css';
+
+function Header(){
+    return(
+        <header class="header">
+            <h2>Smart Expense Tracker</h2>
+        </header>
+    );
+}
+export default Header;
